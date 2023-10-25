41a3bd26c0f46f91dc5a82b85325bcb2fd87bb68
Ltillery03 <113254724+Ltillery03@users.noreply.github.com>
2023-10-25 16:54:47 -0400

    added homework files

---
+++ homework07-3/AVL.java
@@ -0,0 +1,249 @@
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Your implementation of an AVL.
+ *
+ * @author YOUR NAME HERE
+ * @version 1.0
+ * @userid YOUR USER ID HERE (i.e. gburdell3)
+ * @GTID YOUR GT ID HERE (i.e. 900000000)
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+public class AVL<T extends Comparable<? super T>> {
+
+    // Do not add new instance variables or modify existing ones.
+    private AVLNode<T> root;
+    private int size;
+
+    /**
+     * Constructs a new AVL.
+     *
+     * This constructor should initialize an empty AVL.
+     *
+     * Since instance variables are initialized to their default values, there
+     * is no need to do anything for this constructor.
+     */
+    public AVL() {
+        // DO NOT IMPLEMENT THIS CONSTRUCTOR!
+    }
+
+    /**
+     * Constructs a new AVL.
+     *
+     * This constructor should initialize the AVL with the data in the
+     * Collection. The data should be added in the same order it is in the
+     * Collection.
+     *
+     * @param data the data to add to the tree
+     * @throws java.lang.IllegalArgumentException if data or any element in data
+     *                                            is null
+     */
+    public AVL(Collection<T> data) {
+
+    }
+
+    /**
+     * Adds the element to the tree.
+     *
+     * Start by adding it as a leaf like in a regular BST and then rotate the
+     * tree as necessary.
+     *
+     * If the data is already in the tree, then nothing should be done (the
+     * duplicate shouldn't get added, and size should not be incremented).
+     *
+     * Remember to recalculate heights and balance factors while going back
+     * up the tree after adding the element, making sure to rebalance if
+     * necessary.
+     *
+     * Hint: Should you use value equality or reference equality?
+     *
+     * @param data the data to add
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void add(T data) {
+
+    }
+
+    /**
+     * Removes and returns the element from the tree matching the given
+     * parameter.
+     *
+     * There are 3 cases to consider:
+     * 1: The node containing the data is a leaf (no children). In this case,
+     * simply remove it.
+     * 2: The node containing the data has one child. In this case, simply
+     * replace it with its child.
+     * 3: The node containing the data has 2 children. Use the predecessor to
+     * replace the data, NOT successor. As a reminder, rotations can occur
+     * after removing the predecessor node.
+     *
+     * Remember to recalculate heights and balance factors while going back
+     * up the tree after removing the element, making sure to rebalance if
+     * necessary.
+     *
+     * Do not return the same data that was passed in. Return the data that
+     * was stored in the tree.
+     *
+     * Hint: Should you use value equality or reference equality?
+     *
+     * @param data the data to remove
+     * @return the data that was removed
+     * @throws java.lang.IllegalArgumentException if data is null
+     * @throws java.util.NoSuchElementException   if the data is not found
+     */
+    public T remove(T data) {
+
+    }
+
+    /**
+     * Returns the element from the tree matching the given parameter.
+     *
+     * Hint: Should you use value equality or reference equality?
+     *
+     * Do not return the same data that was passed in. Return the data that
+     * was stored in the tree.
+     *
+     * @param data the data to search for in the tree
+     * @return the data in the tree equal to the parameter
+     * @throws java.lang.IllegalArgumentException if data is null
+     * @throws java.util.NoSuchElementException   if the data is not in the tree
+     */
+    public T get(T data) {
+
+    }
+
+    /**
+     * Returns whether or not data matching the given parameter is contained
+     * within the tree.
+     *
+     * Hint: Should you use value equality or reference equality?
+     *
+     * @param data the data to search for in the tree.
+     * @return true if the parameter is contained within the tree, false
+     * otherwise
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public boolean contains(T data) {
+
+    }
+
+    /**
+     * Returns the height of the root of the tree.
+     *
+     * Should be O(1).
+     *
+     * @return the height of the root of the tree, -1 if the tree is empty
+     */
+    public int height() {
+
+    }
+
+    /**
+     * Clears the tree.
+     *
+     * Clears all data and resets the size.
+     */
+    public void clear() {
+
+    }
+
+    /**
+     * The predecessor is the largest node that is smaller than the current data.
+     *
+     * Should be recursive.
+     *
+     * This method should retrieve (but not remove) the predecessor of the data
+     * passed in. There are 2 cases to consider:
+     * 1: The left subtree is non-empty. In this case, the predecessor is the
+     * rightmost node of the left subtree.
+     * 2: The left subtree is empty. In this case, the predecessor is the lowest
+     * ancestor of the node containing data whose right child is also
+     * an ancestor of data.
+     *
+     * This should NOT be used in the remove method.
+     *
+     * Ex:
+     * Given the following AVL composed of Integers
+     *     76
+     *   /    \
+     * 34      90
+     *  \    /
+     *  40  81
+     * predecessor(76) should return 40
+     * predecessor(81) should return 76
+     *
+     * @param data the data to find the predecessor of
+     * @return the predecessor of data. If there is no smaller data than the
+     * one given, return null.
+     * @throws java.lang.IllegalArgumentException if the data is null
+     * @throws java.util.NoSuchElementException   if the data is not in the tree
+     */
+    public T predecessor(T data) {
+
+    }
+
+    /**
+     * Returns the data in the deepest node. If there is more than one node
+     * with the same deepest depth, return the rightmost (i.e. largest) node with
+     * the deepest depth.
+     *
+     * Should be recursive.
+     *
+     * Must run in O(log n) for all cases.
+     *
+     * Example
+     * Tree:
+     *           2
+     *        /    \
+     *       0      3
+     *        \
+     *         1
+     * Max Deepest Node:
+     * 1 because it is the deepest node
+     *
+     * Example
+     * Tree:
+     *           2
+     *        /    \
+     *       0      4
+     *        \    /
+     *         1  3
+     * Max Deepest Node:
+     * 3 because it is the maximum deepest node (1 has the same depth but 3 > 1)
+     *
+     * @return the data in the maximum deepest node or null if the tree is empty
+     */
+    public T maxDeepestNode() {
+
+    }
+
+    /**
+     * Returns the root of the tree.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the root of the tree
+     */
+    public AVLNode<T> getRoot() {
+        // DO NOT MODIFY THIS METHOD!
+        return root;
+    }
+
+    /**
+     * Returns the size of the tree.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the tree
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+}
